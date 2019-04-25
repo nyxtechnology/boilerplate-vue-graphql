31559d735d8d6dd64b33be1eda6ecc2fea3ed9e2
Alan Ktquez <ktquez@gmail.com>
2019-04-25 10:59:04 -0300

    Config apollo client to graphql

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import apolloProvider from '@/apollo/apolloProvider'
 import './registerServiceWorker'
 import '@components/_globals'
 
 Vue.config.productionTip = false
 
 const app = new Vue({
-  router,
   store,
+  router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
 
