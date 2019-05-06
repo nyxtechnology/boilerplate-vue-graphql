651d4faa1885c8c4df73b05160af88291a23f0c1
Alan Ktquez <ktquez@gmail.com>
2019-05-06 14:26:20 -0300

    Add example of basic use of graphql with vue-apollo in Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,60 @@
 <template>
   <div>
-    <img class="logo" alt="" src="@/assets/images/logo.png">
-    <h1>Bem vindo</h1>
+    <h1>Lista de deputados</h1>
+    <ul class="list">
+      <li class="item" v-for="deputados in getDeputados" :key="deputados.node.id">
+        <img class="avatar" :src="deputados.node.urlFoto" :alt="deputados.node.nome">
+        <div>
+          {{ deputados.node.nome }} / {{ deputados.node.siglaPartido }}
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
-  name: 'home'
+  name: 'home',
+  // Configure in .env VUE_APP_GRAPHQL_URL=https://graphql-camara-deputados.herokuapp.com/
+  // to example works
+  apollo: {
+    deputados: gql`query {
+      deputados (first: 20) {
+        edges {
+          node {
+            id
+            nome
+            siglaPartido
+            urlFoto
+          }
+        }
+      }
+    }`
+  },
+  computed: {
+    getDeputados () {
+      if (this.deputados) {
+        return this.deputados.edges
+      }
+      return []
+    }
+  }
 }
 </script>
+
+<style scoped>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+.item {
+  text-align: center;
+}
+.avatar {
+  width: 100px;
+}
+</style>
